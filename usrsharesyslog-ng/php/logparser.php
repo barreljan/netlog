409ee1c75dade8d1a78b23f273f6f57dae798906
barreljan <bartjan@pc-mania.nl>
2022-02-04 21:09:29 +0100

    Rebuild the Logparser (daemon)

--- usrsharesyslog-ng/php/logparser.php
+++ usrsharesyslog-ng/php/logparser.php
@@ -1,115 +1,99 @@
 <?php
-
 // This script will work together with syslog-ng to parse logging information to a MySQL database
-// We will read the logging from a fifo pipe in which syslog-ng with write the syslog info
+// We will read the logging from a fifo pipe in which syslog-ng will write the syslog data
 // Standard syslog-ng - to - MySQL solutions would write all logging to one single table.
-// This script will parse and divide logging into:
-// In this setup logging is divided as per following rules:
-// - A table per day per host for the first 14 days of logging
-// - A table per host per month for the current and last months of logging
-// - Anything beyond last month will be archived
-// This way the most active tables will remain reasonable in size, thus queryable :P
+// This script will parse and divide logging per host, per day.
+// This way the most active tables will have a reasonable size, thus queryable.
 
 // Including logparses variables
 include("/usr/share/syslog-ng/etc/logparser.conf");
 
-$DAY = "";
-$HOST = "";
-
 // Create and check database link
-
-$db_link = mysqli_connect($db_HOST, $db_USER, $db_PASS, $db_NAME);
-if (!$db_link) {
-	die('Could not connect to MySQL server: ' . mysqli_error());
+$db_link = new mysqli($database['HOST'], $database['USER'], $database['PASS'], $database['DB']);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    die();
+}
+if (!$db_link->select_db($database['DB'])) {
+    printf("Unable to select DB: %s\n", mysqli_connect_error());
+    die();
 }
 
-if (!mysqli_select_db($db_link, $db_NAME)) {
-	die('Unable to select DB: ' . mysqli_error());
+openlog("logparser", 0, LOG_LOCAL0);
+
+function create_table($tablename)
+{
+    global $db_link;
+
+    $query = "CREATE TABLE IF NOT EXISTS `$tablename` LIKE template";
+    $createquery = $db_link->prepare($query);
+    $result = $createquery->execute();
+    if (!$result) {
+        // There is a posible serious issue with SQL
+        syslog(LOG_CRIT, "Failed to create table $tablename");
+        die();
+    }
 }
 
-// Check if fifo socket exists
+function parse_log($logitems)
+{
+    global $db_link;
 
-if ( file_exists($log_fifo)) {
-	read_fifo();
-} else {
-	exec("mkfifo $log_fifo", $output, $ret);
-	if ( $ret == "0" ) {
-		read_fifo();
-	} else {
-		die("Unable to create fifo socket: $log_fifo");
-	}
+    // Set defaults
+    $FAC = $PRIO = $LVL = $TAG = $DAY = $TIME = $PROG = $MSG = '';
+
+    foreach ($logitems as $linepart) {
+        $item = explode('_:_', $linepart);
+        if (!isset($item['1'])) {
+            // The field has to have a value, set a blank
+            $item['1'] = '';
+        }
+        ${$item['0']} = trim($item['1']);
+    }
+    // Should the host be missing, default to 'Unidentified Host Object' table
+    $HOST ??= 'UHO';
+
+    // NetLog Scavenger to NetAlert for visability
+    if (false != preg_match('/%LOGSCAVENGER%/', $PROG)) {
+        $HOST = "127.0.0.2";
+    }
+
+    // Construct tablename
+    $HOST_us = str_replace('.', '_', $HOST);
+    $DAY_us = str_replace('-', '_', $DAY);
+    $tablename = 'HST_' . $HOST_us . '_DATE_' . $DAY_us;
+
+    $query = "INSERT INTO $tablename (`HOST`, `FAC`, `PRIO`, `LVL`, `TAG`, `DAY`, `TIME`, `PROG`, `MSG`)
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    if (!$db_link->prepare($query)) {
+        create_table($tablename);
+    }
+    $insertquery = $db_link->prepare($query);
+    $insertquery->bind_param('sssssssss', $HOST, $FAC, $PRIO, $LVL, $TAG, $DAY, $TIME, $PROG, $MSG);
+    $result = $insertquery->execute();
+    if (!$result) {
+        syslog(LOG_ERR, "Failed to insert syslog rule for $HOST: $logitems");
+    }
 }
 
-function read_fifo() {
-	global $log_fifo;
+function read_fifo()
+{
+    global $log_fifo;
 
-	while($fifo = fopen("$log_fifo",'r') ) {
-		$buffer = fgets($fifo);
-		$logitems = explode(' _,_ ', $buffer);
-		parse_log($logitems);
-	}
+    syslog(LOG_INFO, "Opening the fifo and processing syslog messages");
+    while ($fifo = fopen($log_fifo, 'r')) {
+        $buffer = fgets($fifo);
+        $logitems = explode(' _,_ ', $buffer);
+        parse_log($logitems);
+    }
 }
 
-function parse_log($logitems) {
-	global $DAY;
-	global $HOST;
-	global $db_link;
-
-	// Parse items on line
-	$fields = '';
-	$values = '';
-
-	foreach($logitems as $linepart) {
-		$item = explode('_:_',$linepart);
-		if(!isset($item['1'])) { continue; }
-		${$item['0']} = trim($item['1']);
-
-		$fields .= $item['0'] . ", ";
-		$values .= "'" . trim($item['1']) . "',";
-	}
-
-	if(isset($fields) && isset($values)) {
-		$trimmedfields = trim($fields,', ');
-		$trimmedvalues = trim($values,',');
-
-		// Should hostname be empty, we will default to Unidentified Host Object table ;)
-		if($HOST == '') {
-			$HOST = 'UHO';
-		}
-		//echo "INSERT INTO $HOST ($trimmedfields) VALUES ($trimmedvalues)" . "\n";
-
-		unset($fields, $values);
-
-		// NetLog Scavenger to NetAlert for visability
-		if (false != preg_match('/%LOGSCAVENGER%/',$trimmedvalues)) {
-			 $HOST = "127.0.0.2";
-		}
-
-		// Create tablename
-		$HOST_us = str_replace('.','_',$HOST);
-		$DAY_us = str_replace('-','_',$DAY);
-		$tablename = 'HST_' . $HOST_us . '_DATE_' . $DAY_us;
-
-		$query = "INSERT INTO $tablename ($trimmedfields) VALUES ($trimmedvalues)";
-		$result = mysqli_query($db_link, $query);
-
-		if(!$result) {
-			create_table($tablename);
-			$query = "INSERT INTO $tablename ($trimmedfields) VALUES ($trimmedvalues)";
-			$result = mysqli_query($db_link, $query);
-		}
-	}
+// Check if fifo socket exists, otherwise create
+if (!file_exists($log_fifo)) {
+    umask(0);
+    $mode = 0600;
+    posix_mkfifo($log_fifo, $mode);
+    syslog(LOG_NOTICE, "Fifo $log_fifo created");
 }
-
-function create_table($tablename) {
-	global $db_link;
-
-	$query = "CREATE TABLE IF NOT EXISTS $tablename LIKE template";
-	$result = mysqli_query($db_link, $query);
-	if(!$result) {
-		die("Failed to create table $tablename");
-	}
-}
-
-?>
-
+// Process the incomming entries
+read_fifo();
