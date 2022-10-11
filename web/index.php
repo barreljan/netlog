2bd3368bd2200f3b81c5d8b697840d8de06f0fbc
barreljan <bartjan@pc-mania.nl>
2022-10-11 11:35:01 +0200

    Sorting of the lines from the database now correct. Fixes #50

--- web/index.php
+++ web/index.php
@@ -431,7 +431,7 @@ if (!$empty_iplist) {
     if (isset ($lvl_filter)) {
         $query .= "AND `LVL` IN (" . $lvl_filter . ") ";
     }
-    $query .= "ORDER BY `TIME` DESC LIMIT " . $_SESSION['showlines'] . " OFFSET " . $offset;
+    $query .= "ORDER BY `DAY` DESC, `TIME` DESC LIMIT " . $_SESSION['showlines'] . " OFFSET " . $offset;
     $linesquery = $db_link->prepare($query);
     $linesquery->bind_param('s', $searchstring);  // $searchstring is already given the % tags
     $linesquery->execute();
