ed37b36307c5024c38af30a1d93fcf241e695525
barreljan <bartjan@pc-mania.nl>
2022-11-03 15:57:50 +0100

    Removed an unnecessary qualifier.

--- etc/global.php
+++ etc/global.php
@@ -167,7 +167,7 @@ function create_table(string $tablename): void
 if (!function_exists('str_starts_with')) {
     function str_starts_with(string $haystack, string $needle): bool
     {
-        return 0 === strncmp($haystack, $needle, \strlen($needle));
+        return 0 === strncmp($haystack, $needle, strlen($needle));
     }
 }
 if (!function_exists('str_contains')) {
