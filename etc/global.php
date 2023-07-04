b9aaa8f9445f43ef63655765ec84e2d93038b115
barreljan <bartjan@pc-mania.nl>
2023-07-04 16:01:01 +0200

    Bumped version to 3.0.2

--- etc/global.php
+++ etc/global.php
@@ -3,7 +3,7 @@
 // Project: https://github.com/barreljan/netlog
 
 // Versioning etc
-const VERSION = 'v3.0.0';
+const VERSION = 'v3.0.1';
 const NAME = 'Syslog-ng to MySQL parser';
 const AUTHOR = 'bartjan@pc-mania.nl';
 
