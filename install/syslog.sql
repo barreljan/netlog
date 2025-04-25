f6c74aa886381a8393dc08e33f039bb65229b4c7
barreljan <bartjan@pc-mania.nl>
2025-04-25 14:34:53 +0200

    Fixes #86

--- install/syslog.sql
+++ install/syslog.sql
@@ -7,22 +7,20 @@ USE syslog;
 -- Table structure for table `template`
 --
 
-CREATE TABLE `template`
-(
-    `id`   int(15) unsigned NOT NULL AUTO_INCREMENT,
-    `HOST` varchar(39)      NOT NULL,
-    `FAC`  varchar(255)     NOT NULL,
-    `PRIO` varchar(255)     NOT NULL,
-    `LVL`  varchar(255)     NOT NULL,
-    `TAG`  varchar(255)     NOT NULL,
-    `DAY`  varchar(10)      NOT NULL,
-    `TIME` varchar(8)       NOT NULL,
-    `PROG` varchar(255)     NOT NULL,
-    `MSG`  text             NOT NULL,
+CREATE TABLE `template` (
+    `id` int(15) unsigned NOT NULL AUTO_INCREMENT,
+    `HOST` varchar(39) NOT NULL,
+    `FAC` varchar(32) NOT NULL,
+    `PRIO` varchar(32) NOT NULL,
+    `LVL` varchar(16) NOT NULL,
+    `TAG` varchar(64) NOT NULL,
+    `DAY` date NOT NULL,
+    `TIME` time NOT NULL,
+    `PROG` varchar(128) NOT NULL,
+    `MSG` text NOT NULL,
     PRIMARY KEY (`id`),
-    KEY `HOST` (`HOST`),
-    KEY `DAY` (`DAY`),
     KEY `TIME` (`TIME`),
-    KEY `LVL` (`LVL`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = latin1;
+    KEY `LVL` (`LVL`),
+    KEY `PROG` (`PROG`)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=latin1;
